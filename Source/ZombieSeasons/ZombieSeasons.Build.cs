7d409fd91a61f3537f3ff22b6f016655f2049640
Owarino <141052422+Owarino777@users.noreply.github.com>
2026-08-10 22:50:18 +0200

    build: add primary game module build rules

---
+++ Source/ZombieSeasons/ZombieSeasons.Build.cs
@@ -0,0 +1,17 @@
+using UnrealBuildTool;
+
+public class ZombieSeasons : ModuleRules
+{
+    public ZombieSeasons(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore"
+        });
+    }
+}
